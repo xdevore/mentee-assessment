746dca3bd539fd4740b82c838166f33a5f903179
Xavier DeVore <xdevore@haverford.edu>
2024-10-26 00:53:06 -0700

    Initial Commit

---
+++ mentee-assessment/mentee_assessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  mentee_assessmentApp.swift
+//  mentee-assessment
+//
+//  Created by Xavier DeVore on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct mentee_assessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mentee-assessmentUITests/mentee_assessmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mentee_assessmentUITestsLaunchTests.swift
+//  mentee-assessmentUITests
+//
+//  Created by Xavier DeVore on 10/26/24.
+//
+
+import XCTest
+
+final class mentee_assessmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
